a7ad500915134d827a61e8c06dcd827130543dde
Abhiyan001 <abhigyanmahanta001@gmail.com>
2020-11-11 14:14:26 +0530

    add friend list for user

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -4,10 +4,14 @@ class FriendshipsController < ApplicationController
     def create
         @friend_request = Friendship.new(user_id: current_user.id, friend_id: params[:user_id])
         if @friend_request.save
-            redirect_to users_path, notice: 'Friend Request sent!.'
+            redirect_to users_path, notice: 'Friend Request sent!'
         else
-            redirect_to users_path, notice: 'Friend Request not sent!.'
+            redirect_to users_path, notice: 'Friend Request not sent!'
         end
+    end
+
+    def index
+        
     end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_09_125226) do
+ActiveRecord::Schema.define(version: 2020_11_11_083224) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "adminpack"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2020_11_09_125226) do
     t.boolean "confirmed"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["friend_id"], name: "index_friendships_on_friend_id"
     t.index ["user_id"], name: "index_friendships_on_user_id"
   end
